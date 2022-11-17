d5f96911c8079765e1a8a53a2000254ea2f16d1e
Victor Gavrilov <https://gitlab.com/VictorGavrilov>
2022-11-17 18:19:42 +0300

    [Refactor]

--- Assets/Scripts/FigureGenerator.cs
+++ Assets/Scripts/FigureGenerator.cs
@@ -14,12 +14,10 @@ public class Level
 
 public class FigureGenerator : MonoBehaviour
 {
-
     [SerializeField] private Transform parent;
     [SerializeField] private GameObject prefabCube;
     [SerializeField] private List<Vector3> scales;
     [SerializeField] private Vector3 scale;
-    [SerializeField] private bool random;
 
     private List<GameObject> cubes = new List<GameObject>();
     private List<GameObject> xCubes = new List<GameObject>();
@@ -40,6 +38,7 @@ public class FigureGenerator : MonoBehaviour
             Debug.Log("Количество Кубов в фигуре не может быть поделено на 3");
             return;
         }
+
         GenerateFigur();
     }
 
@@ -48,61 +47,58 @@ public class FigureGenerator : MonoBehaviour
         number = 0;
         cubeNumber = 0;
         parent.position = Vector3.zero;
-        float x = 0 - (scale.x / 2f) * prefabCube.transform.localScale.x + prefabCube.transform.localScale.x / 2;
-        float y = 0 - (scale.y / 2f) * prefabCube.transform.localScale.y + prefabCube.transform.localScale.y / 2;
-        int yNumber = 0;
-        float z = 0 - ((scale.z / 2f) * prefabCube.transform.localScale.z) + prefabCube.transform.localScale.z / 2;
-        int zNumber = 0;
+
+        float x = GetFloat(scale.x, prefabCube.transform.localScale.x);
+        float y = GetFloat(scale.y, prefabCube.transform.localScale.y);
+        float z = GetFloat(scale.z, prefabCube.transform.localScale.z);
         float s = scale.x * scale.y * scale.z;
+
+        int yNumber = 0;
+        int zNumber = 0;
+
         for (int i = 0; i < s; i++)
         {
             GameObject cube = Instantiate(prefabCube);
+
             foreach (Text t in cube.GetComponentsInChildren<Text>())
             {
                 t.text = number.ToString();
             }
+
             cubeNumber++;
+
             if (cubeNumber == 3)
             {
                 number++;
                 cubeNumber = 0;
             }
+
             cube.transform.SetParent(parent);
             cube.transform.localPosition = new Vector3(x, y, z);
             xCubes.Add(cube);
-            if (xCubes.Count < scale.x)
+
+            SetOffset(x, y, z, cube);
+
+            if (xCubes.Count == scale.x)
             {
-                Vector3 offset = xCubes[xCubes.Count - 1].transform.localPosition + xCubes[xCubes.Count - 1].transform.localScale;
-                offset.x = x + (xCubes.Count - 1) * prefabCube.transform.localScale.x;
-                offset.y = y;
-                offset.z = z;
-                cube.transform.localPosition = offset;
-            }
-            else if (xCubes.Count == scale.x)
-            {
-                Vector3 offset = xCubes[xCubes.Count - 1].transform.localPosition + xCubes[xCubes.Count - 1].transform.localScale;
-                offset.x = x + (xCubes.Count - 1) * prefabCube.transform.localScale.x;
-                offset.y = y;
-                offset.z = z;
-                cube.transform.localPosition = offset;
                 y += prefabCube.transform.localScale.y;
                 yNumber++;
-                if (yNumber < scale.y)
+
+                if (yNumber == scale.y)
                 {
-                    xCubes.Clear();
-                }
-                else if (yNumber == scale.y)
-                {
-                    y = 0 - (scale.y / 2f) * prefabCube.transform.localScale.y + prefabCube.transform.localScale.y / 2;
+                    y = GetFloat(scale.y, prefabCube.transform.localScale.y);
                     yNumber = 0;
+
                     if (scale.z > 1)
                     {
                         z += prefabCube.transform.localScale.z;
                         zNumber++;
                     }
-                    xCubes.Clear();
                 }
+
+                xCubes.Clear();
             }
+
             cubes.Add(cube);
             AllCubes.Add(cube);
         }
@@ -110,44 +106,16 @@ public class FigureGenerator : MonoBehaviour
         Randomize.RandomizeGenerator(ref cubes, scale);
     }
 
-    private void ShuffleFigure()
+    private float GetFloat (float scale, float prefabScale)
     {
-        if (!random)
-        {
-            return;
-        }
-
-        List<GameObject> objes = new List<GameObject>();
-
-        int x = (int)Mathf.Ceil(scale.y / 2);
-        int i = Random.Range(1, x);
-        float q = Random.Range(1, Mathf.Ceil(scale.y / 2));
-        q *= prefabCube.transform.localScale.y;
-        foreach (GameObject cube in AllCubes)
-        {
-            if (Mathf.Ceil(cube.transform.localPosition.y) == i)
-            {
-                objes.Add(cube);
-            }
-            if (Mathf.Ceil(cube.transform.localPosition.y) == -i)
-            {
-                objes.Add(cube);
-            }
-            if (Mathf.Ceil(cube.transform.localPosition.y) == 0)
-            {
-                objes.Add(cube);
-            }
-        }
-        foreach (GameObject cube in objes)
-        {
-            if (cube.transform.localPosition.y == 0)
-            {
-                cube.transform.position -= new Vector3(q / 2, 0, 0);
-            }
-            else
-            {
-                cube.transform.position += new Vector3(q / 2, 0, 0);
-            }
-        }
+        return 0 - (scale / 2f) * prefabScale + prefabScale / 2;
+    }
+    private void SetOffset(float x, float y, float z, GameObject cube)
+    {
+        Vector3 offset = xCubes[xCubes.Count - 1].transform.localPosition + xCubes[xCubes.Count - 1].transform.localScale;
+        offset.x = x + (xCubes.Count - 1) * prefabCube.transform.localScale.x;
+        offset.y = y;
+        offset.z = z;
+        cube.transform.localPosition = offset;
     }
 }
--- Assets/Scripts/Randomize.cs
+++ Assets/Scripts/Randomize.cs
@@ -8,33 +8,22 @@ public static class Randomize
     private static List<Level> arrays = new List<Level>();
     public static void RandomizeGenerator(ref List<GameObject> cubes, Vector3 scale)
     {
-        Vector3 secondPos = new Vector3();
         List<GameObject> newCubes = new List<GameObject>();
+
         if (scale.x <= 3 || scale.y <= 3 || scale.z <= 3)
         {
             while (cubes.Count > 0)
             {
-                if (newCubes.Count > 0)
-                {
-                    GameObject obj = cubes[Random.Range(0, cubes.Count)];
-                    secondPos = obj.transform.position;
-                    obj.transform.position = newCubes[0].transform.position;
-                    newCubes[0].transform.position = secondPos;
-                    cubes.Remove(obj);
-                    newCubes.Add(obj);
-                }
-                else
-                {
-                    GameObject obj = cubes[Random.Range(0, cubes.Count)];
-                    cubes.Remove(obj);
-                    newCubes.Add(obj);
-                }
+                SetPositions(ref cubes, ref newCubes);
             }
+
             return;
         }
+
         List<GameObject> levelCubes = new List<GameObject>();
         List<GameObject> levelCubesTwo = new List<GameObject>();
         List<Level> newLevels = new List<Level>();
+
         for (int z = 0; z < scale.z; z++)
         {
             arrays.Add(new Level());
@@ -49,49 +38,53 @@ public static class Randomize
         {
             if (arrays.Count >= 2)
             {
-                //Это можно укоротить
                 levelCubes = arrays[0].cubes;
                 levelCubesTwo = arrays[arrays.Count - 1].cubes;
                 levelCubes = levelCubes.Union(levelCubesTwo).ToList();
                 arrays.Remove(arrays[arrays.Count - 1]);
-                arrays.Remove(arrays[0]);
-                Level l = new Level();
-                l.cubes = new List<GameObject>(levelCubes);
-                newLevels.Add(l);
             }
             else
             {
                 levelCubes = arrays[arrays.Count - 1].cubes;
-                arrays.Remove(arrays[0]);
-                Level l = new Level();
-                l.cubes = new List<GameObject>(levelCubes);
-                newLevels.Add(l);
             }
+
+            AddNewLevel(levelCubes, ref newLevels);
+
+            arrays.Remove(arrays[0]);
         }
         foreach (Level level in newLevels)
         {
             int i = 0;
+
             while (level.cubes.Count > 0)
             {
-                if (newCubes.Count > 0)
-                {
-                    GameObject obj = level.cubes[Random.Range(0, level.cubes.Count)];
-                    secondPos = obj.transform.position;
-                    obj.transform.position = newCubes[0].transform.position;
-                    newCubes[0].transform.position = secondPos;
-                    level.cubes.Remove(obj);
-                    newCubes.Add(obj);
-                    i++;
-                }
-                else
-                {
-                    GameObject obj = level.cubes[Random.Range(0, level.cubes.Count)];
-                    level.cubes.Remove(obj);
-                    newCubes.Add(obj);
-                    i++;
-                }
+                SetPositions(ref level.cubes, ref newCubes);
+
+                i++;
             }
         }
+    }
+
+    private static void SetPositions (ref List<GameObject> list, ref List<GameObject> newCubes)
+    {
+        GameObject obj = list[Random.Range(0, list.Count)];
+
+        if (newCubes.Count > 0)
+        {
+            Vector3 secondPos = obj.transform.position;
+            obj.transform.position = newCubes[0].transform.position;
+            newCubes[0].transform.position = secondPos;
+        }
+
+        list.Remove(obj);
+        newCubes.Add(obj);
+    }
+
+    private static void AddNewLevel (List<GameObject> levelCubes, ref List<Level> newLevels)
+    {
+        Level l = new Level();
+        l.cubes = new List<GameObject>(levelCubes);
+        newLevels.Add(l);
     }
 }
 
